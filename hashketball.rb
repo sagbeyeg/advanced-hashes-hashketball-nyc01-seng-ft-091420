5fbdbc9fcc848034dcc7c4dfc13da816d9bd672d
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-10 21:02:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -169,7 +169,7 @@ def player_numbers(team_name)
   output = []  
   game_hash.each do |team, team_info|
       if team_info[:team_name] == team_name
-        team_info[:players].each do |key, value|
+        team_info.each do |key, value|
           if key == [:players]
             value.each do |player|
               outout << player[:number]
