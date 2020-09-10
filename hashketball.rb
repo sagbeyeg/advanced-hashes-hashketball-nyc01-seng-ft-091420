63e174af5e6ea120ab24c9174a06bfe4125347c4
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-10 21:21:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,7 +198,7 @@ def big_shoe_rebounds
   
   game_hash.each do |team, team_info|
     team_info[:player].each do |player|
-      if player[:shoe]
+      if player[:shoe] > player[:shoe]
         return player[:rebounds]
       end
     end
