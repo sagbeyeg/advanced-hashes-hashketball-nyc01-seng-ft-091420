4f22d8af352c84c77f2a66fee15ec0eb1e9e5aea
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-10 20:40:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,3 +164,11 @@ def team_names
     team_info[:team_name]
   end
 end
+
+def player_numbers(team_name)
+  game_hash.map do |team, team_info|
+      if team_info[:team_name] == team_name
+        return team_info[:players][:number]
+      end
+  end
+end
